e5ac109d3040289c96b79e7adb7338c6fa251072
yimijames <148292362+yimijames@users.noreply.github.com>
2024-08-20 22:22:57 -0300

    Add files via upload

---
+++ ProjetoRabbitMQ/consome/consumidor/consome.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+class TemperatureConsumer
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "fila_temperatura",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Recebido {0}", message);
+            };
+            channel.BasicConsume(queue: "fila_temperatura",
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Pressione [enter] para sair.");
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ ProjetoRabbitMQ/produz/produz/produz.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+class TemperatureSensor
+{
+    public static void Main(string[] args)
+    {
+        // Simula uma leitura de temperatura
+        double temperaturaAtual = ObterTemperatura();
+
+        if (temperaturaAtual >= 35)
+        {
+            EnviarMensagem($"Alerta: Temperatura alta! {temperaturaAtual}°C");
+        }
+    }
+
+    private static double ObterTemperatura()
+    {
+        // Simulação de leitura da temperatura
+        Random rand = new Random();
+        return rand.Next(30, 40);
+    }
+
+    private static void EnviarMensagem(string mensagem)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "fila_temperatura",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var body = Encoding.UTF8.GetBytes(mensagem);
+
+            channel.BasicPublish(exchange: "",
+                                 routingKey: "fila_temperatura",
+                                 basicProperties: null,
+                                 body: body);
+            Console.WriteLine(" [x] Enviado {0}", mensagem);
+        }
+    }
+}
